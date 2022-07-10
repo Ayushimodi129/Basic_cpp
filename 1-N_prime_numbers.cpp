6c3daac41bad837d50499a27e1db45717cadd914
Ayushimodi129 <91932910+Ayushimodi129@users.noreply.github.com>
2022-07-10 16:52:39 +0530

    Add files via upload

---
+++ 1-N_prime_numbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath> 
+using namespace std;
+int isprime(int n)
+{int i;
+if(n==1)
+return 0;
+    if (n==2 || n ==3)
+    return 1;
+    if(n%2==0 || n%3==0)
+    return 0;
+    for(i=5;i<=sqrt(n);i=i+6)
+    {
+        if(n%i==0 || n%(i+2)==0)
+        return 0;
+    }
+    return 1;
+}
+
+int main()
+{ 
+    int l,h,i;
+    scanf("%d %d",&l,&h);
+    for(i=l;i<=h;i++)
+    { 
+        if(isprime(i)==1)
+        printf("%d ",i);
+    }
+}
