2365528c82270a7820c5155148d7a5921bad19cc
Ayushimodi129 <91932910+Ayushimodi129@users.noreply.github.com>
2022-08-01 11:07:30 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,i,prod=1;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        prod=prod*i;
+
+}
+    printf("%d",prod);
+}
