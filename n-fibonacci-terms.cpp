c1180ad22ca82b9151f508fc3ea4aed43fafc2fa
Ayushi Modi <91932910+Ayushimodi129@users.noreply.github.com>
2023-11-06 00:05:49 +0530

    Update n-fibonacci-terms.cpp

--- n-fibonacci-terms.cpp
+++ n-fibonacci-terms.cpp
@@ -3,20 +3,21 @@ using namespace std;
 int main()
 { 
     int n,f1=0,f2=1,f3,i,j;
-    scanf("%d",&n);
+    cin>>n;
     if(n==1)
-    cout<<"0";
+        cout<<"0";
     else if (n==2)
-    cout<<"0 1";
-    else{
-        cout<<"0 1 ";
-    for(i=2;i<n;i++)
+        cout<<"0 1";
+    else
     {
-      f3=f1+f2;
-      f1=f2;
-      f2=f3;
-      cout<<f3<<" ";
-        
+        cout<<"0 1 ";
+        for(i=2;i<n;i++)
+            {
+              f3=f1+f2;
+              f1=f2;
+              f2=f3;
+              cout<<f3<<" ";    
+            }
     }
-    }
+    return 0;
     }
