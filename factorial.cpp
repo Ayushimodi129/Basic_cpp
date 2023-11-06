9c69ed95c46f3a184e47093d6f0af9745352f1f5
Ayushi Modi <91932910+Ayushimodi129@users.noreply.github.com>
2023-11-07 00:44:53 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main()
 {
     int n,i,prod=1;
-    scanf("%d",&n);
+    cin>>n;
     for(i=1;i<=n;i++)
     {
         prod=prod*i;
 
-}
-    printf("%d",prod);
+    }
+    cout<<prod;
     return 0;
 }
